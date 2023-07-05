3f6d659b22e3f2c29c021e68d880bb8fe2d950ab
beyersdo <Patrick.Beyersdorffer@reutlingen-university.de>
2023-07-05 12:28:35 +0000

    Frist step for implementing the first extractor.

--- lib/ex_MPY_SPB_SRS.py
+++ lib/ex_MPY_SPB_SRS.py
@@ -13,5 +13,40 @@
     OUTPUT FORMAT:
     ReqStatement[SEP]ID[SEP]Comment[END]
 
-    
 """
+
+from PyPDF2 import PdfReader
+
+#___________________________________________________________________________________________________
+def getTextFromPDF(path_to_doc):
+    """ 
+        Extracts the text from a PDF and converts it in string representation.
+    """
+
+    reader = PdfReader(path_to_doc)
+    output = ""
+    
+    for i in range(0,len(reader.pages) - 1):
+        page = reader.pages[i]
+        output += page.extract_text()
+
+    return output
+
+
+#___________________________________________________________________________________________________
+def getReqsFromText(text):
+    """
+        Splits a given text in single requirements statements.
+        The function is optimized for the MPY-SPB-SRS-0001 document.
+    """
+
+    print(text)
+
+
+#___________________________________________________________________________________________________
+def extract(path_to_doc):
+
+    text = getTextFromPDF(path_to_doc)
+    reqs = getReqsFromText(text)
+
+    print("Success")
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from lib import ex_MPY_SPB_SRS
 
 def main ():
 
-    pass
+    ex_MPY_SPB_SRS.extract("input/MPY-SPB-SRS-001.pdf")
 
 main()
