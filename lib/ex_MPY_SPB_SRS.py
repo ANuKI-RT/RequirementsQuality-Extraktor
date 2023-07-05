8240b6289fcdab57a04c9bb290f7f6dcbc54c4d6
beyersdo <Patrick.Beyersdorffer@reutlingen-university.de>
2023-07-05 10:01:37 +0000

    First functionality for req extraction

--- lib/ex_MPY_SPB_SRS.py
+++ lib/ex_MPY_SPB_SRS.py
@@ -1,5 +1,17 @@
 """
     Extract requirements statements from the MPY-SPB-SRS-001 and resolve acronyms.
 
-    ...
+    The requirements statements in the document are not precise. 
+        - In tabular representation
+        - With many enumerations
+        - With comments in iraltic
+        - With unique identifiers
+        - With source code snipets
+
+    There are empty categories with no requirements (i.e. chapters with no information).
+
+    OUTPUT FORMAT:
+    ReqStatement[SEP]ID[SEP]Comment[END]
+
+    
 """
