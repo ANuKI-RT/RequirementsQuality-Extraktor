6088f23295ce000e424db525e27976909adebe1f
beyersdo <Patrick.Beyersdorffer@reutlingen-university.de>
2023-07-17 14:56:08 +0000

    Third iteration completed.

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def main ():
 
     #ex_MPY_SPB_SRS.extract("input/MPY-SPB-SRS-001.pdf", "input/MPY-SPB-SRS-001 Acronyms.txt")
     #ex_E_CS_SRS.extract("input/E1356-CS-SRS-01_I1_R3.pdf", "input/E1356-CS-SRS-01_I1_R3 Acronyms.txt")
-    ex_E_GTD_SRS.extract("input/E1356-GTD-SRS-01_I1_R4.pdf", "input/E1356-GTD-SRS-01_I1_R4 Acronyms.txt")
+    #ex_E_GTD_SRS.extract("input/E1356-GTD-SRS-01_I1_R4.pdf", "input/E1356-GTD-SRS-01_I1_R4 Acronyms.txt")
 
 main()
