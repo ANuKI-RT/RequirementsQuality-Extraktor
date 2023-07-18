3ff8701fe89adaa5f35559371d0b6598a29d600d
beyersdo <Patrick.Beyersdorffer@reutlingen-university.de>
2023-07-18 17:04:09 +0000

    Fourth iteration completed.

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ def main ():
     #ex_E_CS_SRS.extract("input/E1356-CS-SRS-01_I1_R3.pdf", "input/E1356-CS-SRS-01_I1_R3 Acronyms.txt")
     #ex_E_GTD_SRS.extract("input/E1356-GTD-SRS-01_I1_R4.pdf", "input/E1356-GTD-SRS-01_I1_R4 Acronyms.txt")
     #ex_E_GTD_TR.extract("input/E1356-GTD-TR-01_I2_R1.pdf", "input/E1356-GTD-TR-01-I2-R1 Acronyms.txt")
-    ex_RTEMS_SRS.extract("input/RTEMS_SRS.pdf", "input/RTEMS_SRS Acronyms.txt")
+    #ex_RTEMS_SRS.extract("input/RTEMS_SRS.pdf", "input/RTEMS_SRS Acronyms.txt")
 
 main()
