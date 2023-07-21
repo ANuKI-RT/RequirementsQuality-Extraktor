9fbb4d2a109220a9d98ef6cedbf4aafe8ede4c53
beyersdo <Patrick.Beyersdorffer@reutlingen-university.de>
2023-07-21 07:56:08 +0000

    ICD reqs extracted.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib import ex_E_CS_SRS
 from lib import ex_E_GTD_SRS
 from lib import ex_E_GTD_TR
 from lib import ex_RTEMS_SRS
+from lib import ex_RTEMS_ICD
 
 def main ():
 
@@ -11,5 +12,6 @@ def main ():
     #ex_E_GTD_SRS.extract("input/E1356-GTD-SRS-01_I1_R4.pdf", "input/E1356-GTD-SRS-01_I1_R4 Acronyms.txt")
     #ex_E_GTD_TR.extract("input/E1356-GTD-TR-01_I2_R1.pdf", "input/E1356-GTD-TR-01-I2-R1 Acronyms.txt")
     #ex_RTEMS_SRS.extract("input/RTEMS_SRS.pdf", "input/RTEMS_SRS Acronyms.txt")
+    #ex_RTEMS_ICD.extract("input/RTEMS_ICD.pdf", "input/RTEMS_ICD Acronyms.txt")
 
 main()
