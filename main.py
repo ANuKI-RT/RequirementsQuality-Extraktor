780e39572ec100ddb2234218acff86b5c28f3921
beyersdo <Patrick.Beyersdorffer@reutlingen-university.de>
2023-07-24 08:45:12 +0000

    First SAVOIR lib function implemented.

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from lib import ex_E_GTD_SRS
 from lib import ex_E_GTD_TR
 from lib import ex_RTEMS_SRS
 from lib import ex_RTEMS_ICD
+from lib import ex_SAVOIR_GS
+#from lib import ex_SAVOIR_GS_DSSnIRD
+#from lib import ex_SAVOIR_GS_EPFS
+#from lib import ex_SAVOIR_GS_FCIS
+#from lib import ex_SAVOIR_GS_OBCSRD
+#from lib import ex_SAVOIR_GS_RTUFuOR
 
 def main ():
 
@@ -13,5 +19,12 @@ def main ():
     #ex_E_GTD_TR.extract("input/E1356-GTD-TR-01_I2_R1.pdf", "input/E1356-GTD-TR-01-I2-R1 Acronyms.txt")
     #ex_RTEMS_SRS.extract("input/RTEMS_SRS.pdf", "input/RTEMS_SRS Acronyms.txt")
     #ex_RTEMS_ICD.extract("input/RTEMS_ICD.pdf", "input/RTEMS_ICD Acronyms.txt")
+    
+    #ex_SAVOIR_GS.extract("input/SAVOIR-GS-001.pdf", "input/SAVOIR-GS-001 Acronyms.txt")
+    #ex_SAVOIR_GS_DSSnIRD.extract("input/SAVOIR-GS-DSSnIRD-006.pdf", "input/SAVOIR-GS-DSSnIRD-006 Acronyms.txt")
+    #ex_SAVOIR_GS_EPFS.extract("input/SAVOIR-GS-EPFS-005.pdf", "input/SAVOIR-GS-EPFS-005 Acronyms.txt")
+    #ex_SAVOIR_GS_FCIS.extract("input/SAVOIR-GS-FCIS-002.pdf", "input/SAVOIR-GS-FCIS-002 Acronyms.txt")
+    #ex_SAVOIR_GS_OBCSRD.extract("input/SAVOIR-GS-OBCSRD-008.pdf", "input/SAVOIR-GS-OBCSRD-008 Acronyms.txt")
+    #ex_SAVOIR_GS_RTUFuOR.extract("input/SAVOIR-GS-RTUFuOR-003.pdf", "input/SAVOIR-GS-RTUFuOR-003 Acronyms.txt")
 
 main()
